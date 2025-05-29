4d4f85d9af714603b74d4cb88266a36ca0f0a3c6
KaueKendric <kauekendric270@gmail.com>
2025-05-29 00:32:00 -0300

    Delete PROJETI-2025-1 directory

--- PROJETI-2025-1/setup/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from todos.views import todo_list
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", todo_list),
-]
--- PROJETI-2025-1/todos/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def todo_list(request):
-    nome = "Cleyson"
-    alunos = ["Martelov2", "Pedro pacifico", "Lockless"]
-    return render(request, "todos/todo_list.html", {"nome": nome, "alunos": alunos})
